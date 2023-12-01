c0d41f31de3801767ba3c0d9d45541fd1f25940f
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-01 23:22:46 +0200

    Добавьте файлы проекта.

---
+++ Task1.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введення цілого числа - години
+        Console.Write("13");
+        int h = Convert.ToInt32(Console.ReadLine());
+
+        // Визначення часу доби та виведення відповідного тексту
+        string greeting;
+
+        if (h >= 0 && h < 6)
+        {
+            greeting = "Доброї ночі!";
+        }
+        else if (h >= 6 && h < 12)
+        {
+            greeting = "Доброго ранку!";
+        }
+        else if (h >= 12 && h < 18)
+        {
+            greeting = "Добрий день!";
+        }
+        else
+        {
+            greeting = "Добрий вечір!";
+        }
+
+        Console.WriteLine(greeting);
+    }
+}
